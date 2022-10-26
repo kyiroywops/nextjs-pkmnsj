843d773a5a7f49677e6ad77f8f4aac2f21347144
Kylian Leroy <aleexisazul@gmail.com>
2022-10-25 23:11:39 -0300

    kkkk

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
   return (
-    <>
+    <div>
       <h1>hello</h1>
       <button>hehe</button>
-    </>
+    </div>
     )
 }
 
