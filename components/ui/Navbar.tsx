1e48e7569c23039c2173b9be60017079cbb9b5e2
Kylian Leroy <aleexisazul@gmail.com>
2022-10-31 19:56:22 -0300

    k

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,15 @@
+export const NavBar = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            width: '100%',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'start',
+            padding: '0x 20px',
+            backgroundColor: 'red'
+        }}>
+        <span>kk</span>
+        </div>
+    )
+};
