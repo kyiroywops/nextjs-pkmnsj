72e3fe5afd533bb0b7a680049f897f761b106b30
Kylian Leroy <aleexisazul@gmail.com>
2022-11-04 22:52:57 -0300

    kk

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,8 @@ import { Layout } from '../components/layouts';
 const HomePage: NextPage = () => {
   return (
     <div> 
-      <Layout title='Listado de pokemones'>
+      <Layout title='Listado de pokemones' description='Listado de pokemones'>
+
         <Button>lol</Button>
       </Layout>
     </div>
