8a9deb50aa7881507fb2c271de2449e616ab29e0
Kylian Leroy <aleexisazul@gmail.com>
2022-11-13 23:42:39 -0300

    x

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import {Text, useTheme, Spacer} from '@nextui-org/react';
+import Image from 'next/image';
 
 export const NavBar = () => {
 
@@ -15,11 +16,14 @@ export const NavBar = () => {
             backgroundColor: theme?.colors.gray900.value
 
         }}>
+
+        <Image src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png" alt="icono de la app" width={70} height={70} />
+        
         <Text color='white' h2>P</Text>
         <Text color='white' h3>okemon</Text>
 
         <Spacer css={{flex: 1}} />
-        <Text color='white' h2>Favoritos</Text>
+        <Text color='white'>Favoritos</Text>
         </div>
     )
 };
